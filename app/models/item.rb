d1381e2079bf17ab5d32154077703b633e4cb961
HilotoTaketake <hirotomacbook@gmail.com>
2020-08-05 20:09:19 +0900

    for-furima1

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
   with_options presence: true do
+    validates :image
     validates :name
     validates :explain
     validates :content
